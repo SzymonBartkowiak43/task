f7f07841af915038583ac610961e1781b1ebc53c
Szymon Bartkowiak <szymon.b4310@gmail.com>
2025-02-20 18:10:51 +0100

    f

--- src/main/java/org/example/hackerrang/jeden/Main.java
+++ src/main/java/org/example/hackerrang/jeden/Main.java
@@ -1,4 +1,10 @@
 package org.example.hackerrang.jeden;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+    }
 }
