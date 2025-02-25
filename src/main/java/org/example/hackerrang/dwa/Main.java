1baa107ff3cdf75d0f956e1333ef3d477b0339cd
Szymon Bartkowiak <szymon.b4310@gmail.com>
2025-02-25 21:32:00 +0100

    next

--- src/main/java/org/example/hackerrang/dwa/Main.java
+++ src/main/java/org/example/hackerrang/dwa/Main.java
@@ -1,4 +1,27 @@
 package org.example.hackerrang.dwa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    static boolean isAnagram(String a, String b) {
+        String first = a.toLowerCase();
+        String second = b.toLowerCase();
+
+        Map<Character, Integer> map1 = new HashMap<>();
+        for(char ch: first.toCharArray()) {
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1 );
+        }
+
+        Map<Character, Integer> map2 = new HashMap<>();
+        for(char ch: second.toCharArray()) {
+            map2.put(ch, map2.getOrDefault(ch, 0) + 1 );
+        }
+
+        if(map1.equals(map2)) {
+            return true;
+        }
+        return false;
+
+    }
 }
--- src/main/java/org/example/hackerrang/jeden/Main.java
+++ src/main/java/org/example/hackerrang/jeden/Main.java
@@ -1,10 +1,27 @@
 package org.example.hackerrang.jeden;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String S = in.next();
-        int start = in.nextInt();
-        int end = in.nextInt();
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+
+        String[] parts = s.trim().split("[ !,?._'@]+");
+
+        if(parts.length == 0) {
+            System.out.println("0");
+            return;
+        }
+        if(parts[0].isEmpty()) {
+            System.out.println("0");
+        } else {
+            System.out.println(parts.length);
+            Arrays.stream(parts).forEach(System.out::println);
+        }
+
+
+        scan.close();
     }
 }
